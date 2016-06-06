de206c8ad09d04acd2e05d4ab5ad06d3c4faa39d
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-06-06 12:13:45 -0700

    array of arrays

--- src/js/2048.js
+++ src/js/2048.js
@@ -4,79 +4,71 @@ $(document).ready(function() {
   //Set array to an array of objects with a key of value and number?
 
   var board = [
-    $('#a'),
-    $('#b'),
-    $('#c'),
-    $('#d'),
-    $('#e'),
-    $('#f'),
-    $('#g'),
-    $('#h'),
-    $('#i'),
-    $('#j'),
-    $('#k'),
-    $('#l'),
-    $('#m'),
-    $('#n'),
-    $('#o'),
-    $('#p'),
+    [$('#a'), $('#b'), $('#c'), $('#d')],
+    [$('#e'), $('#f'), $('#g'), $('#h')],
+    [$('#i'), $('#j'), $('#k'), $('#l')],
+    [$('#m'), $('#n'), $('#o'), $('#p')],
     ];
 
-/**/  for (var i = 0; i < board.length; i++) {
-    console.log(board[i].text());
-  }
+// /**/  for (var i = 0; i < 4; i++) {
+//         for (var j = 0; j < 4; i++){
+//           console.log(board[i][j].text());
+//         }
+//       }
 
-/**/  board[1].text(2);
-  console.log(board[3].text());
+/**/  board[0][1].text(2);
+  console.log(board[0][1].text());
 
 
   //background color
   function color(){
-    for (var c = 0; c < board.length; c++){
-      if(board[c].text() === '2') {
-        $(board[c]).addClass('two');
-        $(board[c]).removeClass('default');
+    for(var b = 0; b < 4; b++){
+    for (var c = 0; c < 4; c++){
+      if(board[b][c].text() === '2') {
+        $(board[b][c]).addClass('two');
+        $(board[b][c]).removeClass('default');
       }
-      if(board[c].text() === '4') {
-        $(board[c]).addClass('four');
-        $(board[c]).removeClass('two');
+      if(board[b][c].text() === '4') {
+        $(board[b][c]).addClass('four');
+        $(board[b][c]).removeClass('two');
       }
-      if(board[c].text() === '8') {
-        $(board[c]).addClass('eight');
-        $(board[c]).removeClass('four');
+      if(board[b][c].text() === '8') {
+        $(board[b][c]).addClass('eight');
+        $(board[b][c]).removeClass('four');
       }
-      if(board[c].text() === '16') {
-        $(board[c]).addClass('sixteen');
-        $(board[c]).removeClass('eight');
+      if(board[b][c].text() === '16') {
+        $(board[b][c]).addClass('sixteen');
+        $(board[b][c]).removeClass('eight');
       }
-      if(board[c].text() === '32') {
-        $(board[c]).addClass('thirty-two');
-        $(board[c]).removeClass('sixteen');
+      if(board[b][c].text() === '32') {
+        $(board[b][c]).addClass('thirty-two');
+        $(board[b][c]).removeClass('sixteen');
       }
-      if(board[c].text() === '64') {
-        $(board[c]).addClass('sixty-four');
-        $(board[c]).removeClass('thirty-two');
+      if(board[b][c].text() === '64') {
+        $(board[b][c]).addClass('sixty-four');
+        $(board[b][c]).removeClass('thirty-two');
       }
-      if(board[c].text() === '128') {
-        $(board[c]).addClass('one-twenty-eight');
-        $(board[c]).removeClass('sixty-four');
+      if(board[b][c].text() === '128') {
+        $(board[b][c]).addClass('one-twenty-eight');
+        $(board[b][c]).removeClass('sixty-four');
       }
-      if(board[c].text() === '256') {
-        $(board[c]).addClass('two-fifty-six');
-        $(board[c]).removeClass('one-twenty-eight');
+      if(board[b][c].text() === '256') {
+        $(board[b][c]).addClass('two-fifty-six');
+        $(board[b][c]).removeClass('one-twenty-eight');
       }
-      if(board[c].text() === '512') {
-        $(board[c]).addClass('five-twelve');
-        $(board[c]).removeClass('two-fifty-six');
+      if(board[b][c].text() === '512') {
+        $(board[b][c]).addClass('five-twelve');
+        $(board[b][c]).removeClass('two-fifty-six');
       }
-      if(board[c].text() === '1024') {
-        $(board[c]).addClass('ten-twenty-four');
-        $(board[c]).removeClass('five-twelve');
+      if(board[b][c].text() === '1024') {
+        $(board[b][c]).addClass('ten-twenty-four');
+        $(board[b][c]).removeClass('five-twelve');
       }
-      if(board[c].text() === '2048') {
-        $(board[c]).addClass('twenty-fourty-eight');
-        $(board[c]).removeClass('ten-twenty-four');
+      if(board[b][c].text() === '2048') {
+        $(board[b][c]).addClass('twenty-fourty-eight');
+        $(board[b][c]).removeClass('ten-twenty-four');
       }
+    }
     }
   };
 
@@ -89,28 +81,29 @@ $(document).ready(function() {
     if(keyDirection === 37){
 /**/      console.log(37);
       //checking for empty spaces
-      for (var i = 0; i < board.length; i++) {
-/**/      console.log(board[i].text());
-        if($.trim(board[0].text()).length == 0 &&
-           $.trim(board[1].text()).length == 0 &&
-           $.trim(board[2].text()).length == 0 &&
-           board[3].text().length > 0) {
-          board[0].text(board[3].text());
-          board[3].text('');
+      for (var i = 0; i < 4; i++) {
+        for (var j = 0; j <4; j++){
+/**/      console.log(board[i][j].text());
+        if($.trim(board[0][0].text()).length == 0 &&
+           $.trim(board[0][1].text()).length == 0 &&
+           $.trim(board[0][2].text()).length == 0 &&
+           board[0][3].text().length > 0) {
+          board[0][0].text(board[0][3].text());
+          board[0][3].text('');
         }
-        if($.trim(board[0].text()).length == 0 &&
-           $.trim(board[1].text()).length == 0 &&
-           board[2].text().length > 0) {
-          board[0].text(board[2].text());
-          board[2].text('');
+        if($.trim(board[0][0].text()).length == 0 &&
+           $.trim(board[0][1].text()).length == 0 &&
+           board[0][2].text().length > 0) {
+          board[0][0].text(board[0][2].text());
+          board[0][2].text('');
         }
-        if($.trim(board[0].text()).length == 0 &&
-           board[1].text().length > 0) {
-          board[0].text(board[1].text());
-          board[1].text('');
+        if($.trim(board[0][0].text()).length == 0 &&
+           board[0][1].text().length > 0) {
+          board[0][0].text(board[0][1].text());
+          board[0][1].text('');
         }
         color();
-
+        }
       }
     }
 
