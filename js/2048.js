ed665e840688a3980bccfc9361414467ba234f56
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-06-07 12:17:45 -0700

    fixed double collapsing bug, now found bug with reset

--- js/2048.js
+++ js/2048.js
@@ -195,7 +195,13 @@ var start2 = Math.floor(Math.random()*16);
       startingValues();
       startGame();
     }
-
+    //remove any 'stop' classes
+    for (var i = 0; i < 4; i ++) {
+      for (var j = 0; j < 4; j++) {
+        $(board[i][j]).removeClass('stop');
+        console.log('removed stop')
+      }
+    }
 // get key direction
     var keyDirection = (e.keyCode);
 
@@ -203,10 +209,6 @@ var start2 = Math.floor(Math.random()*16);
     if (keyDirection === 37) {
       for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
-          // remove any 'stop' classes
-          if($(board[i][j]).hasClass('stop')){
-            $(board[i][j]).removeClass('stop');
-           }
           // if value in index 1
           if ($.trim(board[i][1].text()).length > 0 &&
             $.trim(board[i][0].text()).length === 0) {
@@ -244,18 +246,20 @@ var start2 = Math.floor(Math.random()*16);
             board[i][3].text('');
           }//
           // start of checking side by side numbers
-          if (board[i][0].text() === board[i][1].text() && board[i][0].text() !== '' && !$(board[i][0]).hasClass('stop')) {
+          if (board[i][0].text() === board[i][1].text() && board[i][0].text() !== '' && !$(board[i][1]).hasClass('stop')) {
             board[i][0].text(board[i][0].text()*2);
             board[i][1].text('');
             $(board[i][0]).addClass('stop');
           }
-          if (board[i][1].text() === board[i][2].text() && board[i][1].text() !== '') {
+          if (board[i][1].text() === board[i][2].text() && board[i][1].text() !== '' && !$(board[i][2]).hasClass('stop')) {
             board[i][1].text(board[i][1].text()*2);
             board[i][2].text('');
+            $(board[i][1]).addClass('stop');
           }
-          if (board[i][2].text() === board[i][3].text() && board[i][2].text() !== '') {
+          if (board[i][2].text() === board[i][3].text() && board[i][2].text() !== '' && !$(board[i][3]).hasClass('stop')) {
             board[i][2].text(board[i][2].text()*2);
             board[i][3].text('');
+            $(board[i][2]).addClass('stop');
           }
 
         } // end second for loop
@@ -268,10 +272,6 @@ var start2 = Math.floor(Math.random()*16);
     else if (keyDirection === 38) {
       for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
-          // remove any 'stop' classes
-            if($(board[i][j]).hasClass('stop')){
-              $(board[i][j]).removeClass('stop');
-            }
           // if value in index 1
           if ($.trim(board[1][j].text()).length > 0 &&
             $.trim(board[0][j].text()).length === 0) {
@@ -310,18 +310,20 @@ var start2 = Math.floor(Math.random()*16);
             board[3][j].text('');
           }//
           // start of checking side by side numbers
-          if (board[0][j].text() === board[1][j].text() && board[0][j].text() !== '' && !$(board[0][j]).hasClass('stop')) {
+          if (board[0][j].text() === board[1][j].text() && board[0][j].text() !== '' && !$(board[1][j]).hasClass('stop')) {
             board[0][j].text(board[0][j].text()*2);
             board[1][j].text('');
             $(board[0][j]).addClass('stop');
           }
-          if (board[1][j].text() === board[2][j].text() && board[1][j].text() !== '') {
+          if (board[1][j].text() === board[2][j].text() && board[1][j].text() !== '' && !$(board[2][j]).hasClass('stop')) {
             board[1][j].text(board[1][j].text()*2);
             board[2][j].text('');
+            $(board[1][j]).addClass('stop');
           }
-          if (board[2][j].text() === board[3][j].text() && board[2][j].text() !== '') {
+          if (board[2][j].text() === board[3][j].text() && board[2][j].text() !== '' && !$(board[3][j]).hasClass('stop')) {
             board[2][j].text(board[i][2].text()*2);
             board[3][j].text('');
+            $(board[2][j]).addClass('stop');
           }
         } // end second for loop
       } //  end first for loop
@@ -333,10 +335,6 @@ var start2 = Math.floor(Math.random()*16);
     else if (keyDirection === 39){
       for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
-           // remove any 'stop' classes
-            if($(board[i][j]).hasClass('stop')){
-              $(board[i][j]).removeClass('stop');
-            }
           if ($.trim(board[i][2].text()).length > 0 &&
             $.trim(board[i][3].text()).length === 0) {
               board[i][3].text(board[i][2].text());
@@ -368,18 +366,20 @@ var start2 = Math.floor(Math.random()*16);
           board[i][0].text('');
           }
           // start of checking side by side numbers
-          if (board[i][3].text() === board[i][2].text() && board[i][3].text() !== '' && !$(board[i][3]).hasClass('stop')) {
+          if (board[i][3].text() === board[i][2].text() && board[i][3].text() !== '' && !$(board[i][2]).hasClass('stop')) {
             board[i][3].text(board[i][3].text()*2);
             board[i][2].text('');
             $(board[i][3]).addClass('stop');
           }
-          if (board[i][2].text() === board[i][1].text() && board[i][2].text() !== '') {
+          if (board[i][2].text() === board[i][1].text() && board[i][2].text() !== '' && !$(board[i][1]).hasClass('stop')) {
             board[i][2].text(board[i][2].text()*2);
             board[i][1].text('');
+            $(board[i][2]).addClass('stop');
           }
-          if (board[i][1].text() === board[i][0].text() && board[i][1].text() !== '') {
+          if (board[i][1].text() === board[i][0].text() && board[i][1].text() !== '' && !$(board[i][0]).hasClass('stop')) {
             board[i][1].text(board[i][1].text()*2);
             board[i][0].text('');
+            (board[i][1]).addClass('stop');
           }
         } // end second for loop
       } //  end first for loop
@@ -391,10 +391,6 @@ var start2 = Math.floor(Math.random()*16);
     else if (keyDirection === 40){
       for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
-            // remove any 'stop' classes
-            if($(board[i][j]).hasClass('stop')){
-              $(board[i][j]).removeClass('stop');
-            }
           if ($.trim(board[2][j].text()).length > 0 &&
             $.trim(board[3][j].text()).length === 0) {
               board[3][j].text(board[2][j].text());
@@ -426,18 +422,20 @@ var start2 = Math.floor(Math.random()*16);
             board[0][j].text('');
           }
           // start of checking side by side numbers
-          if (board[3][j].text() === board[2][j].text() && board[3][j].text() !== '' && !$(board[3][j]).hasClass('stop')) {
+          if (board[3][j].text() === board[2][j].text() && board[3][j].text() !== '' && !$(board[2][j]).hasClass('stop')) {
             board[3][j].text(board[3][j].text()*2);
             board[2][j].text('');
             $(board[3][j]).addClass('stop');
           }
-          if (board[2][j].text() === board[1][j].text() && board[2][j].text() !== '') {
+          if (board[2][j].text() === board[1][j].text() && board[2][j].text() !== '' && !$(board[1][j]).hasClass('stop')) {
             board[2][j].text(board[2][j].text()*2);
             board[1][j].text('');
+            $(board[2][j]).addClass('stop');
           }
-          if (board[1][j].text() === board[0][j].text() && board[1][j].text() !== '') {
+          if (board[1][j].text() === board[0][j].text() && board[1][j].text() !== '' && !$(board[0][j]).hasClass('stop')) {
             board[1][j].text(board[1][j].text()*2);
             board[0][j].text('');
+            $(board[1][j]).addClass('stop');
           }
         } // end second for loop
       } //  end first for loop
